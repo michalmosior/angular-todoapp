b55f1bd43580c84b48449658f693200f906b15f7
michalmosior <michal.mosior@gmail.com>
2023-05-07 15:56:09 +0200

    add addTaskInput validation

--- src/app/components/add-task-form/add-task-form.component.ts
+++ src/app/components/add-task-form/add-task-form.component.ts
@@ -1,5 +1,4 @@
 import { Component, EventEmitter, Output } from '@angular/core';
-
 import { Task } from 'src/app/Task';
 
 @Component({
@@ -12,11 +11,16 @@ export class AddTaskFormComponent {
   description: string = '';
   completed: boolean = false;
 
+  validate = false;
+
   onSubmit(e: Event): void {
     e.preventDefault();
     if (!this.description) {
+      this.validate = true;
+      console.log(this.validate);
       return;
     } else {
+      this.validate = false;
       const newTask: Task = {
         description: this.description,
         completed: this.completed,
